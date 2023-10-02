8e40f2efdf38fdef7d93b40babbb5f6c22f0accf
Frida Fernhede <fernhedefrida@gmail.com>
2023-10-02 13:02:03 +0200

    Added Styles.css, data.php and updated totalTeams.php

--- totalTeams.php
+++ totalTeams.php
@@ -1,1 +1,7 @@
- should echo the total number of teams in the data
+  <?php
+  //this file should echo the total number of teams in the data
+ require __DIR__."/data.php"; ?>
+<h3> Number of teams:<?php
+$totalTeams= count($teams);
+echo $totalTeams;
+?> <h3>
